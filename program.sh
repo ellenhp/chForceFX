f696fcf3f4da67ec7e62a7a3a9a78873858cd284
Nolan Poe <nopoe.dev@gmail.com>
2015-12-19 23:08:40 -0800

    Checked in reset.py (ommitted in last commit) and added a convenience script for programming.

---
+++ program.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+# You may need to run this as root if reset.py needs to detach a kernel driver.
+
+make;
+python reset.py;
+sleep 1;
+avrdude -v -p atmega32u4 -c avr109 -P /dev/ttyACM0 -b 57600 -D -U flash:w:Joystick.hex:i;
